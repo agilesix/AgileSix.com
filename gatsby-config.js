6f2e8e5d6d70e12866027ccea0217c3ee04c8b5a
Hondo <hi@hondo.co>
2019-05-05 13:49:30 -0400

    Adds Netlify CMS

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ module.exports = {
     author: `@gatsbyjs`,
   },
   plugins: [
+    `gatsby-plugin-netlify-cms`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
