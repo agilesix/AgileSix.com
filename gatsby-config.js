43b7712150b520a9467b8d60dc09347d8b605387
Hondo <hi@hondo.co>
2019-06-14 13:14:26 -0400

    Starts on team

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,6 +28,13 @@ module.exports = {
     {
       resolve: 'gatsby-source-filesystem',
       options: {
+        path: `${__dirname}/src/content/team`,
+        name: 'team-members',
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
         path: `${__dirname}/src/content/case-studies`,
         name: 'casestudies',
       },
--- src/content/team/robert-rasmussen.md
+++
@@ -1,7 +0,0 @@
----
-templateKey: team-member
-title: Robert Rasmussen
-position: Co-Founder & CEO
-featuredimage: /img/0.jpeg
----
-
