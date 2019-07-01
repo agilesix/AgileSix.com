98af97fd48069f8dd12ddc8c6657116981fb15de
Hondo <hi@hondo.xyz>
2019-06-30 22:09:29 -0400

    Update capabilities

--- src/templates/index.js
+++ src/templates/index.js
@@ -25,7 +25,7 @@ export const IndexTemplate = ({
       <p className={'text-xl md:text-2xl mb-10'}>Agile Six helps government agencies and other clients create customized digital solutions to meet the needs of their users. No matter how much experience you have with digital development or where you are in the process, we can get you where you want to go.</p>
       <Capabilities featuredOnly={true} />
       <div class="text-center mt-10">
-        <a href="/capabilities" className="block md:inline-block px-8 py-3 leading-none border text-white text-center bg-red hover:border-red hover:text-red hover:bg-white mt-4 md:mt-0">Learn More</a>
+        <a href="/capabilities" className="block md:inline-block px-8 py-3 leading-none border text-white text-center bg-red hover:border-red hover:text-red hover:bg-white mt-4 md:mt-0">Learn about our capabilities</a>
       </div>
     </Block>
     <Block className={'bg-grey-light'} title={'Recent Work'}>
