f60dc437f039aada6727a2b33757d232463263c2
Hondo <hi@hondo.xyz>
2019-07-11 20:43:15 -0400

    Cleans up form fields on contact

--- src/templates/contact.js
+++ src/templates/contact.js
@@ -22,20 +22,20 @@ export const ContactTemplate = ({
       <form class="w-full max-w-lg mx-auto" data-netlify="true">
         <div class="w-full px-3 mb-6">
           <label class="block uppercase tracking-wide text-gray-700 text-xs mb-2" for="name"><strong>Name</strong></label>
-          <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="name" type="text" placeholder="" />
+          <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-blue-dark" id="name" name="name" type="text" placeholder="" />
         </div>
         <div class="w-full px-3 mb-6">
           <label class="block uppercase tracking-wide text-gray-700 text-xs mb-2" for="email"><strong>Email</strong></label>
-          <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="email" type="email" placeholder="" />
+          <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-blue-dark" id="email" name="email" type="email" placeholder="" />
         </div>
         <div class="w-full px-3 mb-6">
-          <label class="block uppercase tracking-wide text-gray-700 text-xs mb-2" for="email"><strong>Company</strong> (optional)</label>
-          <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="email" type="email" placeholder="" />
+          <label class="block uppercase tracking-wide text-gray-700 text-xs mb-2" for="company"><strong>Company</strong> (optional)</label>
+          <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-blue-dark" id="company" name="company" type="text" placeholder="" />
         </div>
         <div class="w-full px-3 mb-6">
-          <label class="block uppercase tracking-wide text-gray-700 text-xs mb-2" for="email"><strong>I'd like to talk about</strong></label>
-          <div class="inline-block relative w-full">
-            <select class="block appearance-none w-full bg-white border border-grey-light hover:border-grey-dark px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
+          <label class="block uppercase tracking-wide text-gray-700 text-xs mb-2" for="subject"><strong>I'd like to talk about</strong></label>
+          <div class="relative">
+            <select id="subject" name="subject" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-blue-dark">
               <option>Select one...</option>
               <option value="hiring us">Working with us</option>
               <option value="market development">Civic tech market development</option>
@@ -49,7 +49,7 @@ export const ContactTemplate = ({
         </div>
         <div class="w-full px-3 mb-6">
           <label class="block uppercase tracking-wide text-gray-700 text-xs mb-2" for="email"><strong>Company</strong> (optional)</label>
-          <textarea rows="10" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="message" type="message" placeholder=""></textarea>
+          <textarea rows="10" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-blue-dark" id="message" type="message" placeholder=""></textarea>
           <div class="text-center mt-10">
             <button className="block md:inline-block px-8 py-3 leading-none border text-white text-center bg-red hover:border-red hover:text-red hover:bg-white mt-4 md:mt-0">Send it</button>
           </div>
