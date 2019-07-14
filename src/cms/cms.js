b95c625beaf816497f28323468ad2a7a4de8d797
Hondo <hi@hondo.xyz>
2019-07-14 08:12:31 -0400

    Adds basic about page preview

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -3,7 +3,9 @@ import CMS from 'netlify-cms-app'
 import IndexPagePreview from './preview-templates/IndexPagePreview'
 import TeamPagePreview from './preview-templates/TeamPagePreview'
 import WorkPagePreview from './preview-templates/WorkPagePreview'
+import AboutPagePreview from './preview-templates/AboutPagePreview'
 
 CMS.registerPreviewTemplate('work', WorkPagePreview)
 CMS.registerPreviewTemplate('team', TeamPagePreview)
 CMS.registerPreviewTemplate('index', IndexPagePreview)
+CMS.registerPreviewTemplate('about', AboutPagePreview)
