6956cd1787786c5e8304e03391274baa669e5767
Hondo <hi@hondo.xyz>
2019-07-14 20:39:36 -0400

    Fix config error

--- gatsby-config.js
+++ gatsby-config.js
@@ -91,7 +91,7 @@ module.exports = {
         display: `minimal-ui`,
         icon: `src/images/agile-six-icon.png`, // This path is relative to the root of the site.
       }
-    }
-    `gatsby-plugin-offline`,
+    },
+    `gatsby-plugin-offline`
   ],
 }
