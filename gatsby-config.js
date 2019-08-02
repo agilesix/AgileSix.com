8e90b49b61a38bb0100a57f31edba33ff8b03ddb
Matthew Crist <matt@govbloom.com>
2019-08-02 08:40:59 -0400

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -98,11 +98,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Agile Six`,
+        short_name: `Agile Six`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#ffffff`,
+        theme_color: `#ffffff`,
         display: `minimal-ui`,
         icon: `src/images/agile-six-icon.png`, // This path is relative to the root of the site.
       }
