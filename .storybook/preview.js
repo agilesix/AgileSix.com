e0a0b45ab61b2bc752af2b62ac11e7bbc435c62b
Daniel Miller <daniel.miller@agile6.com>
2020-03-27 10:40:35 -0700

    import global css for storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,6 +2,8 @@ import { addDecorator } from "@storybook/react"
 import { withA11y } from "@storybook/addon-a11y"
 import { action } from "@storybook/addon-actions"
 
+import "../src/css/global.css"
+
 // Gatsby's Link overrides:
 // Gatsby defines a global called ___loader to prevent its method calls from creating console errors you override it here
 global.___loader = {
