ac6d794eb1ac696912fc968dfe8e4eda8af687b9
Daniel Miller <daniel.miller@agile6.com>
2022-11-15 20:45:18 -0800

    remove client-side sorting experiment for blog posts, be keep created and midified timestamps in case we need later

--- src/components/blog-posts.js
+++ src/components/blog-posts.js
@@ -22,6 +22,8 @@
                html
                fields {
                  slug
+                 gitModifiedTime
+                 gitCreatedTime
                }
                frontmatter {
                  title
@@ -41,13 +43,7 @@
        }
      `}
     render={(data) => {
-      let blogposts = [ ...data.allMarkdownRemark.edges].sort((a, b) => {
-        // TODO -- sort blog posts outside of query until we can find a reliable way to sort
-        // blog posts by publish or modified date in query
-        const timeA = a.node.fields.gitModifiedTime;
-        const timeB = b.node.fields.gitModifiedTime;
-        return (timeA, timeB) => (timeA < timeB) ? -1 : ((timeA > timeB) ? 1 : 0);
-      });
+      const blogposts = data.allMarkdownRemark.edges;
        return (
          <div className={'flex flex-wrap md:-mx-2'}>
            {blogposts.map(blogpost => {
