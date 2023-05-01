8094af6503f90a8a54ab3930e066b73f92c0f8ab
Daniel Miller <daniel.miller@agile6.com>
2023-05-01 15:48:50 -0700

    update color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,31 +1,31 @@
 // tailwind.config.js
 module.exports = {
-  darkMode: 'class',
+  darkMode: "class",
   content: [
-    './src/**/*.{js,jsx,ts,tsx}',
-    './src/pages/**/*.{js,jsx,ts,tsx}',
-    './src/components/**/*.{js,jsx,ts,tsx}',
-    './src/templates/**/*.{js,jsx,ts,tsx}',
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/pages/**/*.{js,jsx,ts,tsx}",
+    "./src/components/**/*.{js,jsx,ts,tsx}",
+    "./src/templates/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     colors: {
-      white: '#ffffff',
-      red: '#cc091c',
+      white: "#ffffff",
+      red: "#8A2A2B",
       grey: {
-        light: '#ecf0f1',
-        default: '#cbd5e0',
-        medium: '#666464'
+        light: "#D9D9D6",
+        default: "#cbd5e0",
+        medium: "#707372",
       },
       blue: {
-        light: '#00b7ef',
-        dark: '#002360'
+        light: "#41B6E6",
+        dark: "#003A70",
       },
-      transparent: 'transparent'
+      transparent: "transparent",
     },
     fontFamily: {
-      'agile6': ['Source Sans Pro', '-apple-system', 'BlinkMacSystemFont'],
-    }
+      agile6: ["Source Sans Pro", "-apple-system", "BlinkMacSystemFont"],
+    },
   },
   variants: {},
-  plugins: []
+  plugins: [],
 }
