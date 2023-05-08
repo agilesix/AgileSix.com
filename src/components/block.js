b8df7394a5a5f741c092cdfa0118e6687652bbc9
Daniel Miller <daniel.miller@agile6.com>
2023-05-08 15:31:58 -0700

    fix link color contrast and remaining axe test failures found while testing

--- src/components/block.js
+++ src/components/block.js
@@ -3,13 +3,25 @@ import React from "react"
 import Break from "./break"
 import SectionTitle from "./section-title"
 
-const Block = ({ title, children, className, image, imageTitle, imageBackgroundColor }) => (
+const Block = ({
+  title,
+  children,
+  className,
+  image,
+  imageTitle,
+  imageBackgroundColor,
+}) => (
   <div className={`px-6 py-10 md:py-20 relative ${className}`}>
-    <div className={'max-w-5xl mx-auto'}>
+    <div className={"max-w-5xl mx-auto"}>
       {title && (
-        <div className={'text-center'}>
-          <SectionTitle>{title}</SectionTitle>
-          <Break className={'mx-auto border-red'} />
+        <div className={"text-center"}>
+          {typeof title === "string" && title.trim() && (
+            <SectionTitle>{title}</SectionTitle>
+          )}
+          {title && typeof title !== "string" && (
+            <SectionTitle>{title}</SectionTitle>
+          )}
+          <Break className={"mx-auto border-red"} />
         </div>
       )}
       {children}
--- src/components/section-title.js
+++ src/components/section-title.js
@@ -1,7 +1,13 @@
 import React from "react"
 
-const SectionTitle = ({textColor, children}) => (
-  <div className={`text-3xl md:text-4xl leading-tight font-bold ${textColor || 'text-blue-dark'}`}>{children}</div>
+const SectionTitle = ({ textColor, children }) => (
+  <h2
+    className={`text-3xl md:text-4xl leading-tight font-bold ${
+      textColor || "text-blue-dark"
+    }`}
+  >
+    {children}
+  </h2>
 )
 
 export default SectionTitle
